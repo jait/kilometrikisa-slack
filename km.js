5a4e614ccc16631ec8879842806bebbb5c3c59b6
Jari Tenhunen <jari.tenhunen@boogiesoftware.com>
2024-04-15 15:04:47 +0300

    Commit latest changes.

--- km.js
+++ km.js
@@ -6,6 +6,15 @@ const settings = require('./settings');
 
 const seriesNames = {"small": "Piensarja", "electric": "Sähkösarja"};
 
+async function postMessage(messageData) {
+	//console.log(JSON.stringify(messageData));
+	await axios.post(settings.SLACK_WEBHOOK_URL, messageData);
+}
+
+function formatFloat(value) {
+	return value.toLocaleString(settings.SLACK_LOCALE);
+}
+
 function formatSeries(seriesData) {
 	const seriesName = seriesNames[seriesData.series];
 	if (!seriesName) {
@@ -13,7 +22,7 @@ function formatSeries(seriesData) {
 	}
 	return {
 		"type": "mrkdwn",
-		"text": `*${seriesName}*\nSijoitus: *${seriesData.seriesPlacement}*\nJoukkueen keskiarvo: *${seriesData.distancePerPerson} km*\nKilometrit yhteensä: *${seriesData.totalDistance}*`
+		"text": `*${seriesName}*\nSijoitus: *${seriesData.seriesPlacement}*\nJoukkueen keskiarvo: *${formatFloat(seriesData.distancePerPerson)} km*\nKilometrit yhteensä: *${formatFloat(seriesData.totalDistance)}*`
 	};
 }
 
@@ -35,17 +44,79 @@ function formatTeamData(teamData) {
 	}
 }
 
+function formatTopCyclistMessage(topCyclist) {
+	const totalKm = topCyclist.distanceByRegularBike + topCyclist.distanceByEbike;
+	return {
+		"text": `Viikon polokija on *${topCyclist.name}*, jolle matkaa kertyi yhteensä ${formatFloat(totalKm)} km! :tada:`
+	};
+}
+
 async function postTeamStats() {
 	const teamData = await km.getTeamStatistics(settings.TEAM_SLUG);
 	//console.log(teamData);
 	//console.log(formatTeamData(teamData));
 	if (teamData) {
-		//console.log(JSON.stringify(formatTeamData(teamData)));
-		await axios.post(settings.SLACK_WEBHOOK_URL, formatTeamData(teamData));
+		await postMessage(formatTeamData(teamData));
 	}
 	else {
 		console.warn(`No team data for ${settings.TEAM_SLUG}`);
 	};
 }
 
+async function postTopCyclist() {
+	const session = await km.kilometrikisaSession({username: settings.KMKISA_USERNAME, password: settings.KMKISA_PASSWORD});
+	const contest = await km.getLatestContest();
+	const currentStats = await session.getTeamMemberStatistics(settings.TEAM_SLUG, contest.slug);
+	//console.log(JSON.stringify(currentStats));
+	// fetch previous stats from somewhere
+	previousStats = {};
+	// save currentStats somewhere
+	const topCyclist = getTopCyclist(currentStats, previousStats);
+	console.log(topCyclist);
+	await postMessage(formatTopCyclistMessage(topCyclist));
+}
+
+function getTopCyclist(currentStats, previousStats) {
+	let regKmByName = {};
+	let eKmByName = {};
+	let topCyclist;
+	let topDistance = 0;
+
+	// what is this shit?
+	function getName(element) {
+		const name = element.fullName || element.name;
+		if (typeof name === 'string' || name instanceof String) {
+			return name;
+		} else {
+			// "name":{"subItem":"Janis Petke","value":""}
+			return name.subItem;
+		}
+	}
+
+	currentStats.distanceStatistics.forEach((el) => {
+		const name = getName(el);
+		regKmByName[name] = el.distanceByRegularBike || 0;
+		eKmByName[name] = el.distanceByEbike || 0;
+	});
+
+	if (previousStats && previousStats.distanceStatistics) {
+		previousStats.distanceStatistics.forEach((el) => {
+			const name = getName(el);
+			regKmByName[name] = regKmByName[name] - el.distanceByRegularBike;
+			eKmByName[name] = eKmByName[name] - el.distanceByEbike;
+		});
+	}
+
+	// secret rating algorithm
+	for (const [name, km] of Object.entries(regKmByName)) {
+		let dist = km + eKmByName[name] / 2;
+		if (dist > topDistance) {
+			topCyclist = name;
+			topDistance = dist;
+		}
+	}
+	return {name: topCyclist, adjustedDistance: topDistance, distanceByRegularBike: regKmByName[topCyclist], distanceByEbike: eKmByName[topCyclist]};
+}
+
 exports.postTeamStats = postTeamStats;
+exports.postTopCyclist = postTopCyclist;
--- settings.js
+++ settings.js
@@ -2,7 +2,10 @@
 
 const settings = {
     TEAM_SLUG: process.env.TEAM_SLUG,
-    SLACK_WEBHOOK_URL: process.env.SLACK_WEBHOOK_URL
+    SLACK_WEBHOOK_URL: process.env.SLACK_WEBHOOK_URL,
+    KMKISA_USERNAME: process.env.KMKISA_USERNAME,
+    KMKISA_PASSWORD: process.env.KMKISA_PASSWORD,
+    SLACK_LOCALE: "fi-FI"
 }
 
 module.exports = settings;
